c9f98711f3aff8ecbd59826d3e3a956a1c4c96b0
udav-py <93492256+udav-py@users.noreply.github.com>
2021-11-01 12:50:25 +0200

    Add files via upload

---
+++ Tg_bot_2/Program.cs
@@ -0,0 +1,105 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Tg_bot_2
+{
+    class Program
+    {
+        private static string token { get; } = "2020421106:AAG8XZjrNS-rlL9bMgElTCCjdB5_gtu9ru0";
+        private static TelegramBotClient client;
+            static List<string> repl = new List<string>();
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+            client = new TelegramBotClient(token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+
+        }
+
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null)
+            {
+                Console.WriteLine(msg.Text);
+                
+                switch (msg.Text)
+                {
+                    case "/start":
+                        await client.SendTextMessageAsync(msg.Chat.Id, msg.Text, replyMarkup: GetButtons());
+                        break;
+                    case "Рапорт майно":
+                        repl.Clear();
+                        client.OnMessage -= OnMessageHandler;
+                        await client.SendTextMessageAsync(msg.Chat.Id,"Воинская часть:", replyMarkup: new ForceReplyMarkup { Selective = true } );
+                        client.OnMessage += OnReplyHandler;
+                        break;
+                }
+
+            }
+
+        }
+
+        private static async void OnReplyHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+
+            if(msg.ReplyToMessage != null && msg.ReplyToMessage.Text.Contains("Воинская часть:"))
+            {
+                repl.Add(msg.Text);
+                await client.SendTextMessageAsync(msg.Chat.Id, "Имя фамилия (Например: О.Петров):", replyMarkup: new ForceReplyMarkup { Selective = true });
+            }
+            else if(msg.ReplyToMessage != null && msg.ReplyToMessage.Text.Contains("Имя фамилия (Например: О.Петров):"))
+            {
+                repl.Add(msg.Text);
+                await client.SendTextMessageAsync(msg.Chat.Id, "Звание ФИО:", replyMarkup: new ForceReplyMarkup { Selective = true });
+            }
+            else if(msg.ReplyToMessage != null && msg.ReplyToMessage.Text.Contains("Звание ФИО:"))
+            {
+                repl.Add(msg.Text);
+                await client.SendTextMessageAsync(msg.Chat.Id, "Дата:", replyMarkup: new ForceReplyMarkup { Selective = true });
+            }
+            else if(msg.ReplyToMessage != null && msg.ReplyToMessage.Text.Contains("Дата:"))
+            {
+                repl.Add(msg.Text);
+            }
+
+            if(repl.Count == 4)
+            {
+                WordManager.Replacer(repl,0);
+                
+                using (var fileStream = File.OpenRead(WordManager.copyRaport))
+                {
+                    await client.SendDocumentAsync(msg.Chat.Id, fileStream);
+                }
+            }
+        }
+
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "Рапорт майно" } },
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "Рапорт виплата"}
+                }
+            }
+            };
+        }
+    }
+}
+
+
---
+++ Tg_bot_2/WordManager.cs
@@ -0,0 +1,108 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tg_bot_2
+{
+    class WordManager
+    {
+        public static string origRaport;
+        public static string copyRaport;
+
+        public enum TypesOfDoc
+        {
+            Рапорт_майно,
+            Рапорт_виплата,
+            Рапорт_травма
+
+        }
+
+        public static void pathMaker(int type)
+        {
+            
+            string sourceRaport = ((WordManager.TypesOfDoc)type).ToString();
+            int numerator = int.Parse(File.ReadAllText(@"C:\Users\Cadet\OneDrive\Робочий стіл\proj\Копии\Numerator.txt"));
+            origRaport = $@"C:\Users\Cadet\OneDrive\Робочий стіл\proj\Шаблоны\" + sourceRaport + ".docx";
+            copyRaport = $@"C:\Users\Cadet\OneDrive\Робочий стіл\proj\Копии\" + sourceRaport + "_" + numerator.ToString() + ".docx";
+
+
+            File.Delete(@"C:\Users\Cadet\OneDrive\Робочий стіл\proj\Копии\Numerator.txt");
+            numerator++;
+            File.WriteAllText(@"C:\Users\Cadet\OneDrive\Робочий стіл\proj\Копии\Numerator.txt", numerator.ToString());
+
+
+        }
+
+        public static void CopyMaker(string source, string copy)
+        {
+            using (WordprocessingDocument mainDoc = WordprocessingDocument.Open(source, true))
+            {
+                using (var resultDoc = WordprocessingDocument.Create(copy,
+                  WordprocessingDocumentType.Document))
+                {
+                    foreach (var part in mainDoc.Parts)
+                        resultDoc.AddPart(part.OpenXmlPart, part.RelationshipId);
+                }
+
+            }
+        }
+
+        public static void Replacer(List<string> replaceStrings, int type)
+        {
+            pathMaker(type);
+            CopyMaker(origRaport,copyRaport);
+
+            string[] charsToReplace = { "!", "@", "#", "%", "^", "&", "\\*", "\\(", "\\)", "-" };
+            string replaceText = null;
+            using (WordprocessingDocument mainDoc = WordprocessingDocument.Open(copyRaport, true))
+            {
+                string docText = null;
+                using (StreamReader sr = new StreamReader(mainDoc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+                int length = charsToReplace.Length > replaceStrings.Count ? replaceStrings.Count : charsToReplace.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    if (isDate(replaceStrings[i]))
+                    {
+                        replaceText = DateMaker(replaceStrings[i]);
+                    }
+                    else
+                    {
+                        replaceText = replaceStrings[i];
+                    }
+
+                    Regex regexText = new Regex(charsToReplace[i]);
+                    docText = regexText.Replace(docText, replaceText);
+                }
+
+
+                using (StreamWriter sw = new StreamWriter(mainDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(docText);
+                }
+
+
+            }
+        }
+
+
+        public static string DateMaker(string date)
+        {
+            Regex reg = new Regex(@"\s*(\d*)\s*(\D*)\s*(\d*)");
+            Match mtch = reg.Match(date);
+            return "«" + mtch.Groups[1].ToString() + "» " + mtch.Groups[2].ToString() + " " + mtch.Groups[3].ToString() + "р.";
+        }
+
+        public static bool isDate(string date) => new Regex(@"\s*(\d+)\s*(\D+)\s*(\d+)").IsMatch(date);
+
+
+    }
+}
